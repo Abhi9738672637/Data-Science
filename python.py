de76df0f1b3b2bb8876bf61fc8faeca411a86115
Abhi9738672637 <abhishek.ns198@gmail.com>
2024-04-08 15:41:01 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+n1=int(input("enter the first number:"))
+n2=int(input("enter the second number:))
+             if n>n2:
+             print("biggest number is:",n1)
+             else:
+             print("biggest number is:",n2)
+
+
+             
+
+             
